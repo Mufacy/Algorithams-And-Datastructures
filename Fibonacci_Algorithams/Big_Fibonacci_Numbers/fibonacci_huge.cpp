49dd3c99d073d1721fa35e0e0fd9126f50893874
Null-Edge <nulledge.it@gmail.com>
2021-03-05 21:00:41 +0200

    Fibonacci of big numbers
    fixed the pisano period counting method

--- Fibonacci_Algorithams/Big_Fibonacci_Numbers/fibonacci_huge.cpp
+++ Fibonacci_Algorithams/Big_Fibonacci_Numbers/fibonacci_huge.cpp
@@ -19,40 +19,46 @@ long long get_fibonacci_huge_naive(long long n, long long m)
 
 long long get_fibonacci_huge_fast(long long n, long long m)
 {
-    int pieN;
-    if (m <= 3)
-        pieN = m;
+    if (n <= 1)
+        return n;
 
-    if (m > 3)
+    long prev_pisano = 0;
+    long cur_pisano = 1;
+    long Pisano_Leng = 1;
+    long tmp;
+
+    for (int i = 0; i < m * m; ++i)
     {
-        if (m % 2 == 0)
+        tmp = cur_pisano;
+        cur_pisano = (prev_pisano + cur_pisano) % m;
+        prev_pisano = tmp;
+
+        if ((cur_pisano == 1) && (prev_pisano == 0))
         {
-            pieN = 2 * m;
-        }
-        else
-        {
-            pieN = (4 * m) + 3;
+            Pisano_Leng = i + 1;
+            break;
         }
     }
 
-    int remainder = n % pieN;
+    int remainder = n % Pisano_Leng;
 
-    long long previous = 0;
-    long long current = 1;
+    long long Fib_previous = 0;
+    long long Fib_current = 1;
+    long long tmp_previous;
 
     for (long long i = 0; i < remainder - 1; ++i)
     {
-        long long tmp_previous = previous;
-        previous = current;
-        current = (tmp_previous + current) % m;
+        tmp_previous = Fib_previous;
+        Fib_previous = Fib_current;
+        Fib_current = (tmp_previous + Fib_current) % m;
     }
-    return current;
+
+    return Fib_current % m;
 }
 
 int main()
 {
     long long n, m;
     std::cin >> n >> m;
-    std::cout << get_fibonacci_huge_fast(n, m) << "\n";
-    //std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    std::cout << get_fibonacci_huge_fast(n, m) << '\n';
 }
